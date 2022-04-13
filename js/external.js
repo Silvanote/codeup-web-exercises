1700c62659e98f47a07a3e41faaa0428c1a32388
Juan Silva <juan.silvawd@gmail.com>
2022-04-13 16:10:23 -0500

    April 13 code

---
+++ js/external.js
@@ -0,0 +1,16 @@
+/*
+//JS Exercise #2
+
+console.log("Hello from external Javascript");
+
+"use script"
+alert ("Welcome to my Website!");
+let decision = prompt("What is your favorite color?");
+alert (decision + " is my favorite color too.");
+*/
+//JS Exercise #3
+alert ("Which movie would you like to watch?");
+let moviedecision = prompt(" Great movie, for how many days?")
+alert (moviedecision + " days, do you have a premium membership?")
+
+
---
+++ js/test.js
@@ -0,0 +1,5 @@
+"use strict";
+
+console.log("This is from an included file!");
+
+let
