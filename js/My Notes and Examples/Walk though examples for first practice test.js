cbd6a33f4cdb0f896f34e563c6a587be771c61fd
Juan Silva <juan.silvawd@gmail.com>
2022-04-20 14:23:44 -0500

    new commit

---
+++ js/My Notes and Examples/Walk though examples for first practice test.js
@@ -0,0 +1,54 @@
+function isFalse(example){
+    return example === false;
+
+}
+
+function isTrue(trueexample){
+    return trueexample === true;
+
+}
+
+function not(not){
+    return !not;
+}
+
+function addOne(extraone){
+    return parseFloat(extraone) + 1
+}
+
+function isEven(num) {
+    if (isNaN(parseFloat(num))) {
+        return false;
+    } else {
+        return parseFloat(num) % 2 === 0;
+    }
+
+    function isIdentical(y1, y2) {
+        return y1 === y2;
+    }
+
+    function isEqual(x1, x2) {
+        return x1 == x2;
+    }
+
+    function or(x, y) {
+        return x || y;
+    }
+}
+    function and(x, y) {
+        return x && y;
+    }
+}
+    function concat(x, y){
+        return "" + con + cat
+    }
+}
+    // another example concat
+    // function concat (con, cat){
+//  return con.tostring() + cat.tostring();
+
+// another example
+// function concat(con,cat){
+// return "" + con + cat;
+//}
+
