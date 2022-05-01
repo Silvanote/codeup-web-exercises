8742022e93e0b72b11d7ef520e16ac786e99cb9d
Juan Silva <juan.silvawd@gmail.com>
2022-05-01 00:36:15 -0500

    late practice

--- js/Retake practice.js
+++ js/Retake practice.js
@@ -1,59 +1,86 @@
-"use strict"
+// "use strict"
+//
 
-//practice 29 and 30 april
-function analyzeColor(color){
-    if(color == "blue"){
-        return "Blue is the color of the sky."
-    }else if(color == "red") {
-        return "Red means something is hot."
-    }else if(color == "green"){
-        return "Money is green.";
-    }else{
-        return "I don't know anything about that color."
-    }
+// // More practice
+// let names = ["first seat","second seat", "third seat", "fourth seat"];
+//
+// // for (let i = 0; i < names.length; i++){
+// //     console.log(names[0]);
+// //     // console.log(names[i]);
+// //     //this one gave me "first seat" four time in a row
+//
+// //
+// // for (name of names){
+// //     console.log(name);
+// //}
+// //     let user = {firstName: "John", lastName:"Doe" };
+// // for (users in user);
+// // console.log(user[users]);
+// //
+//
+
+//
+
+// function analyzeColor(color){
+//     if(color == "blue"){
+//         return "Blue is the color of the sky."
+//     }else if(color == "red") {
+//         return "Red means something is hot."
+//     }else if(color == "green"){
+//         return "Money is green.";
+//     }else{
+//         return "I don't know anything about that color."
+//     }
+// }
+// console.log(analyzeColor("blue"))
+// console.log(analyzeColor("green"))
+// console.log(analyzeColor("azul"))
+//
+// for(let i = 0; i < 5; i++){
+//     console.log("loop" + i);
+//     if (i == 3) break;
+// }
+// for(let i = 9; i > 0; i--){
+//     console.log("loop" + i);
+// }
+// let names = ["juan", "damon", "kevin", "dalton"];
+//
+// for (let i = 0; i < names.length; i++){
+//     console.log(names[i]);
+// }
+// function parseNumber(input) {
+//     if (!isNaN(parseFloat(input))) {
+//         return parseFloat(input);
+//     } else
+//         return false;
+// }
+// console.log(parseNumber(5));
+// console.log(parseNumber("7"));
+// console.log(parseNumber("five"));
+//
+//
+//     function isANumber(value) {
+//         return !isNaN(parseFloat(value));
+//     }
+//
+//     console.log(isANumber("xx"))
+//     console.log(isANumber(17));
+//
+//     function increment(num) {
+//         if (isANumber(num)) {
+//             return parseNumber(num) + 1;
+//         } else {
+//             return false;
+//         }
+//     }
+
+//     console.log(increment(6));
+//     console.log(increment('8lkjhhjkhjk7'));
+//github
+function isExample(input){
+If(typeof input !=="string")|| !isNaN(parseNumber(str));
+    Return: false
+    } else {
+    Str=str.toLowerCase();
+    Return str ==str.split("").reverse().join("");
 }
-console.log(analyzeColor("blue"))
-console.log(analyzeColor("green"))
-console.log(analyzeColor("azul"))
-
-for(let i = 0; i < 5; i++){
-    console.log("loop" + i);
-    if (i == 3) break;
-}
-for(let i = 9; i > 0; i--){
-    console.log("loop" + i);
-}
-let names = ["juan", "damon", "kevin", "dalton"];
-
-for (let i = 0; i < names.length; i++){
-    console.log(names[i]);
-}
-function parseNumber(input) {
-    if (!isNaN(parseFloat(input))) {
-        return parseFloat(input);
-    } else
-        return false;
-}
-console.log(parseNumber(5));
-console.log(parseNumber("7"));
-console.log(parseNumber("five"));
-
-
-    function isANumber(value) {
-        return !isNaN(parseFloat(value));
-    }
-
-    console.log(isANumber("xx"))
-    console.log(isANumber(17));
-
-    function increment(num) {
-        if (isANumber(num)) {
-            return parseNumber(num) + 1;
-        } else {
-            return false;
-        }
-    }
-
-    console.log(increment(6));
-    console.log(increment('8lkjhhjkhjk7'));
-
