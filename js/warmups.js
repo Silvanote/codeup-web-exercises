b5c449d610545e572f3534c18fd388a65190d71f
Juan Silva <juan.silvawd@gmail.com>
2022-05-03 14:01:48 -0500

    flexbox 1

--- js/warmups.js
+++ js/warmups.js
@@ -104,11 +104,80 @@
 //     }); return sum;
 // }
 //Another answer:
-function sumOfElements(arr) {
-    let bucket = 0
-    for (let el of arr) {
-        bucket += el
-    }
-    return bucket;
+// function sumOfElements(arr) {
+//     let bucket = 0
+//     for (let el of arr) {
+//         bucket += el
+//     }
+//     return bucket;
+// }
+// console.log(sumOfElements(1,2,3,4,5));
+
+// May 3 Warm up
+// Create a function, returnProductEssentialDetails, that takes in a product object and returns a simpler product object containing only the product name and price in cents.
+//
+//     Examples...
+
+let product1 = {
+    name: 'Hammer',
+    priceInCents: 400,
+    description: 'It is a a hammar.',
+    inventory: 25034
 }
-console.log(sumOfElements(1,2,3,4,5));
+let product2 = {
+    name: 'Computer',
+    priceInCents: 40000,
+    description: 'It is a computer.',
+    inventory: 33000
+}
+let product3 = {
+    name: 'Ruler',
+    priceInCents: 1000,
+    description: 'It is a ruler.',
+    inventory: 2200
+}
+function returnProductEssentialDetails(product) {
+    return {
+        name: product.name,
+        priceInCents: product.priceInCents
+    }
+
+    returnProductEssentialDetails(product1); // returns...
+    /*
+        {
+            name: 'Hammar',
+            priceInCents: 400
+        }
+    */
+    returnProductEssentialDetails(product2); // returns...
+    /*
+        {
+            name: 'Computer',
+            priceInCents: 40000
+        }
+    */
+    returnProductEssentialDetails(product3); // returns...
+    /*
+        {
+            name: 'Ruler',
+            priceInCents: 1000
+        }
+    */
+//
+
+// BONUS (if time permits)
+//
+// Create a function, returnAllProductsEssentialDetails, that takes in an array of product objects and returns an array of product objects containing only the essential details of the product name and price in cents.
+//BONUS Answer
+    let products = [product1, product2, product3];
+
+    function returnAllProductEssentialDetails(products) {
+        let productsOutput = [];
+        products.forEach(function (product) {
+            products.push(returnProductEssentialDetails(product));
+        });
+    }
+
+    return productsOutput;
+}
+console.log(returnProductEssentialDetails(products));
