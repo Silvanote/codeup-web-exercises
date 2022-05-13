b54fb64823958a4cb8dce48785b9de7fd8021ae4
Juan Silva <juan.silvawd@gmail.com>
2022-05-13 08:41:10 -0500

    more exercise updates

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,39 +1,21 @@
 "use strict"
 
 //
-//
-function ask(askforNumber) {
+function ask() {
     do {
         let askForNumber = prompt("Please give me a number between 1 and 50.");
-    if(askForNumber <=50 && askForNumber % 2 !== 0 ){
-        break;
+        if (askForNumber <= 50 && askForNumber % 2 !== 0) {
+            break;
+        }
     }
-}
-    while(true);
-for (let i = 1; i<=50; i += 2) {
-    if (i === askForNumber) {
-        console.log("Got the number" + askForNumber);
-        continue;
-    }
-    console.log(i);
-}
-ask();}
 
-// (function () {
-//     let userNum;
-//
-//     while (true) {
-//         userNum = parseFloat(prompt("Give me an odd number between 1 and 50, please."));
-//         if (userNum % 2 !== 0 && userNum >= 1 && userNum <= 50) {
-//             break;
-//         }
-//     }
-//
-//     for(let j = 1; j <= 50; j += 2) {
-//         if (j === userNum) {
-//             console.log("WHOOP! Skipping " + userNum);
-//             continue;
-//         }
-//         console.log(j);
-//     }
-// })();
+    while (true);
+    for (let i = 1; i <= 50; i += 2) {
+        if (i === askForNumber) {
+            console.log("Got the number" + askForNumber);
+            continue;
+        }
+        console.log(i);
+    }
+}
+ask();
--- js/while.js
+++ js/while.js
@@ -1,47 +1,25 @@
-// multiplyBy2Alot();
-// let i = 2;
-// while (i < 65537) {
-//     console.log(i);
-//     i+=i;
-// }
-//This is how you get a random number between 50 and 100
+"use script"
 
-let allCones = Math.floor(Math.random() * 51) + 50;
-
-do{
-    // This expression will generate a random number between 1 and 5
-    let customerOrder = Math.floor(Math.random() * 5) + 1;
-    console.log("Customer orders" + customerOrder + "cone(s). We have " + allCones + "left in stock.");
-    if (customerOrder > allCones) {
-        console.log("Order cannot be fulfilled. NEXT!");
-    } else {
-        allCones -= customerOrder;
-        console.log("here you go! We not have" + allCones + "left in stock.");
+function keepOnMultiplying () {
+    let num = 1;
+    while (num <= 65537) {
+        num*=2;
+        console.log(num);
     }
-} while (allCones > 0);
-
-console.log("Whew! What a day. Sold all the cones.")
-//
-function iceCreamTruckProgram () {
-    // This is how you get a random number between 50 and 100
-    let allCones = Math.floor(Math.random() * 51) + 50;
-    let numOfCustomers = 0;
-
-    do {
-        numOfCustomers++;
-        // This expression will generate a random number between 1 and 5
-        let customerOrder = Math.floor(Math.random() * 5) + 1;
-        console.log("Customer orders " + customerOrder + " cone(s). We have " + allCones + " left in stock.");
-        if (customerOrder > allCones) {
-            console.log("Order cannot be fulfilled. NEXT!");
-        } else {
-            allCones -= customerOrder;
-            console.log("Here you go! We now have " + allCones + " left in stock.");
-        }
-    } while (allCones > 0);
-
-    console.log("Whew! What a day... Sold out of all my cones. And it only took " + numOfCustomers + " customers.");
+}
+function iceCreamPurchases() {
+    let iceCreamOrders = Math.ceil(Math.random() * 50)+ 49;
+    var startPurchases = 0;
+    do{
+        startPurchases++;
+        let iceCreamOrders = Math.random(Math.ceil() * 5) + 1;
+    if(iceCreamOrders > allStock){
+    console.log("Not enough inventory to complete order, sorry.") + prompt("Please provide your email and we will contact you once more inventory is in stock.")}
+    } while(
+    allStock > iceCreamOrders)
+    console.log("Thank you for your purchase, only" + allStock + "are left to purchase.");
 }
 
-iceCreamTruckProgram();
-//Had no idea how to do this loop!
+console.log("We have no inventory left at this time.")
+
+    iceCreamPurchases();
