62b91c49c7af413fcfa60323883747dc867587bd
Juan Silva <juan.silvawd@gmail.com>
2022-05-19 16:53:44 -0500

    end of day push

--- js/dom-practice.js
+++ js/dom-practice.js
@@ -1,11 +1,11 @@
 
 // TODO: Prompt the user to enter a color. Select the first div with the class of "question", and change the font color to the users input.
-let choice = prompt("Pleast enter a color.");
+let choice = prompt("Please enter a color.");
 
-// let div1 = document.getElementsByClassName("question")[0];
-// console.log(document.getElementsByClassName("question"));
-//
-// div1.style.color = choice;
+let div1 = document.getElementsByClassName("question")[0];
+console.log(document.getElementsByClassName("question"));
+
+div1.style.color = choice;
 
 let divider = document.getElementsByClassName
 
@@ -31,4 +31,44 @@ document.getElementById("changeColor") //change color came from the ID(which can
     let newColor = document.getElementById("color").value;
     document["body"],["style"],["backgroundColor"] = newColor;
 });
-//should i use commas between arrays???? Ask question on comma and if I have to use brackets or can use objects and pull information?
+// May 19
+//Should I use commas between arrays???? Ask question on comma and if I have to use brackets or can use objects and pull information?
+//// 1. Create a button with the id of addTodo. Create an input of type text with the id of "todo"
+// // 2. Create an unordered list with the id="todosList"
+// // 4. When the add button is clicked: (step 1)
+// //       a. Make it such that the value from the input is added to a list items.
+// //          example: <li>input</li> inside of <ul>
+//   //       b. Reset the value in the addTodos input.
+//   // 5. If the value for the input is empty. Prevent it from creating another <li> element
+//   white_check_mark (step 2) and (step 3)
+//   +1
+//   raised_hands
+//CORRECT ANSWER:
+let toDoElement= document.getElementById("todo");
+let toDoList = document.getElementById("todosList");
+let toDoButton = document.getElementById("addTodo");
+
+toDoButton.addEventListener("click", function (){
+    if (toDoElement.value !== ""){
+        let str = "<li>" + toDoElement.value + "</li>";
+        toDoList.innerHTML = str;
+        toDoElement.value = "";
+    }
+});
+//
+// document.getElementById("todolist").addEventListener(("click",function (){
+// let li = document.getElementById('todosList').addEventListener()
+//     for(let i=0; i<li.length; i++){
+//     li[i] <h3>U+2713 JavaScript</h3>
+// }
+
+//     let li = document.getElementsByTagName("li");
+// console.log(li);
+//
+// console.log(li[1]);
+// li[1].textContent = "Hello 2";
+// li[1].style.fontWeight = "bold";
+// li[2].style.fontWeight ="bold";
+// for (let i=0; i<li.length;i++){
+//     li[i].style.backgroundColor = "lightblue";
+// }
---
+++ js/intro-to-jquery.js
@@ -0,0 +1,27 @@
+// $(function(){
+//      //Stuff goes here and remember $ = Jquery()function. Reduces time
+//     alert("This is the first alert for Title");
+//
+//     $("ul").css("background", "lightgrey");
+//
+//   $("li").css("border", "1px red solid");
+// })
+
+
+//David's solution -->
+<!--$(function(){-->
+<!--    let introToList = $("#introToList").html();-->
+<!--    alert(pContent);-->
+<!--});-->
+
+//$(function(){-->
+<!--$(".doggysFaveFoods").css("border", "1px solid #d00");-->
+<!--});-->
+
+// $("li").css("font-size", "20px");
+//
+// $("h1, p, li").css("background", "yellow");
+//
+// $("h1").each(function(index,el){
+//    alert($(el).html());})
+
