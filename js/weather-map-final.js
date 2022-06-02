5576d2a8cf697c819f89fd38add714e49555ced1
Juan Silva <juan.silvawd@gmail.com>
2022-06-02 14:38:38 -0500

    today's practice

--- js/weather-map-final.js
+++ js/weather-map-final.js
@@ -26,5 +26,27 @@ function displayResults(weather){
 
     let now = new Date();
     let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
 
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span> °F </span>;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innterText = weather.weather[0].main;
+    
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${weather.main.temp_min} °F / ${weather.main.temp_max} °`
+}
+
+function dateBuilder(d) {
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
 }
