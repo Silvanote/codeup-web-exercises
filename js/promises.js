2691d6548d47eb8af1df4a65304d1fba5f641d8a
Juan Silva <juan.silvawd@gmail.com>
2022-06-06 20:24:39 -0500

    updated files and images

--- js/promises.js
+++ js/promises.js
@@ -1,31 +1,56 @@
 "use strict"
 
-fetch(url, {headers: {'Authorization': 'GITHUB_TOKEN'}})
+let URL = "https://api.github.com/users/{username}/events/public"
 
-// Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made. Reference the github api documentation to achieve this.
-// Octokit.js
-// https://github.com/octokit/core.js#readme
+fetch(url, {headers: {'Authorization': `token ${GITHUB_TOKEN}`}})
+    .then(res => res.json())
+    .then(results => results[0])
+    .then(firstEvent => console.log(firstEvent))
 
-    .then((responses) => responses.json())
-    .then((data) => {
-        console.log(data)
-    })
+const commitsUploaded = (username) => {
+    const URL2 = `https://api.github.com/users/${username}/events/public`
 
+    return fetch(URL2, {headers: {'Authorization': `token ${GITHUB_TOKEN}`}})
+        .then(res => res.json())
+        .then(results => results[0])
+        .then(firstEvent => console.log(firstEvent.created_at))
+        .catch(error => console.error(error))
+}
+commitsUploaded('Silvanote');
 
-const octokit = new Octokit({
-    auth: 'GITHUB_TOKEN'
-})
-
-await octokit.request('GET /users/{Silvanote}/received_events', {
-    username: 'Silvanote'
-})
-const URL = "https://api.github.com/users/{Silvanote}/events/public";
-
-fetch('https://api.github.com/users').then( response => {
-    response.json().then( users => {
-        users.forEach( user => {
-            // do something with each user object...
-            console.log(user);
-        });
-    });
-});
+// fetch('https://api.github.com/users')
+// .then( response => {response.json()
+// .then( users => { users.forEach( user => {
+//  //   do something with each user object...
+//             console.log(user);
+//         });
+//     });
+// });
+//
+// // Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made. Reference the github api documentation to achieve this.
+// // Octokit.js
+// // https://github.com/octokit/core.js#readme
+//
+//     .then((responses) => responses.json())
+//     .then((data) => {
+//         console.log(data)
+//     })
+//
+//
+// const octokit = new Octokit({
+//     auth: 'GITHUB_TOKEN'
+// })
+//
+// await octokit.request('GET /users/{Silvanote}/received_events', {
+//     username: 'Silvanote'
+// })
+// const URL = "https://api.github.com/users/{Silvanote}/events/public";
+//
+// fetch('https://api.github.com/users').then( response => {
+//     response.json().then( users => {
+//         users.forEach( user => {
+//             // do something with each user object...
+//             console.log(user);
+//         });
+//     });
+// });
